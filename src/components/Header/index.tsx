4ca301391bf028381a7bd32bb92b10d1f158a83e
Gabriel Nascimento <127701444+bielnascf@users.noreply.github.com>
2025-04-15 17:28:30 -0300

    Add and fix Desktop and Mobile Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import HeaderDesktop from "./HeaderDesktop";
+import HeaderMobile from "./HeaderMobile";
+
+export default function Header() {
+
+  return (
+    <>
+      <HeaderDesktop />
+      <HeaderMobile />
+    </>
+  );
+}
