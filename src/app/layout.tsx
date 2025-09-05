bdf1957981177c3f5bdd1c6def15546dcfca5931
Gabriel Nascimento <127701444+bielnascf@users.noreply.github.com>
2025-09-05 16:08:35 -0300

    refactor: Implementei o ClientProviders

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/Providers/ThemeProvider";
-import Gradient from "@/components/Gradient";
-import { ConditionalLayout } from "@/components/ConditionalLayout";
-import Header from "@/components/Header";
-import { Toaster } from "@/components/ui/toaster";
+import { ClientProviders } from "@/providers/ClientProviders";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -17,30 +13,26 @@ export const metadata: Metadata = {
   description:
     "Simplify and organize your life with All-in-One: a personal organization app that brings together task management, financial tracking, study planning, quick notes, and an online diary",
   icons: {
-    icon: '/favicon.svg',
+    icon: "/favicon.svg",
   },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html
-      className="scroll-smooth scrollbar dark"
+      className="scrollbar dark"
       lang="en"
-      suppressHydrationWarning={true}
+      data-scroll-behavior="smooth"
+      suppressHydrationWarning
     >
-      <body className={`${inter.className}`}>
-        <ThemeProvider>
-          <Gradient />
-          <ConditionalLayout>
-            <Header />
-            {children}
-          </ConditionalLayout>
-        </ThemeProvider>
-        <Toaster />
+      <body className={inter.className}>
+        <ClientProviders>
+          {children}
+        </ClientProviders>
       </body>
     </html>
   );
