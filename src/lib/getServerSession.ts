da8a18907ce870a8a6980a1de6122542fdd0f090
Gabriel Nascimento <127701444+bielnascf@users.noreply.github.com>
2025-09-05 16:32:42 -0300

    other: configs da aplicação all-in-one

--- src/lib/getServerSession.ts
+++
@@ -1,8 +0,0 @@
-import { headers } from 'next/headers';
-import { auth } from '@/lib/auth';
-
-export async function getServerSession(){
- return await auth.api.getSession({
-  headers: await headers(),
- });
-}
