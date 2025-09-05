e5b15c1d78780c5396fd41fe5d0e50fc32876c46
Gabriel Nascimento <127701444+bielnascf@users.noreply.github.com>
2025-09-05 16:13:31 -0300

    feat:Adicionei stats relacionadas as tasks do usuário

---
+++ src/app/dashboard/taskPlanner/_components/TaskStats/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { TaskStatsProps } from "@/interfaces/ITasks";
+import { BadgeCheckIcon, ClipboardListIcon, HourglassIcon } from "lucide-react";
+
+export default function TaskStats({ total, completed, pending }: TaskStatsProps) {
+  return (
+    <div className="flex sm:gap-4 gap-2 items-center">
+      <div className="text-zinc-300">
+        <span className="font-bold flex items-center gap-1">
+          <ClipboardListIcon className="h-4 w-4"/>
+          {total ?? 0}
+        </span>
+      </div>
+      <div className="text-white">
+        <span className="font-bold flex items-center gap-1 text-green-500">
+          <BadgeCheckIcon className="h-4 w-4"/>
+          {completed ?? 0}
+        </span>
+      </div>
+      <div className="text-white">
+        <span className="font-bold flex items-center gap-1 text-yellow-500">
+          <HourglassIcon className="h-4 w-4"/>
+          {pending ?? 0}
+        </span>
+      </div>
+    </div>
+  );
+}
--- src/app/dashboard/taskPlanner/page.tsx
+++ src/app/dashboard/taskPlanner/page.tsx
@@ -1,18 +1,17 @@
-import { getServerSession } from "@/lib/getServerSession";
-import { redirect } from "next/navigation";
+import { protectRoute } from "@/lib/serverSession";
+import TaskContainer from "./_components/TaskContainer";
 
-export default function TaskPlanner() {
-  const session = getServerSession();
+export default async function TaskPlanner() {
+  await protectRoute();
 
-  if(!session) {
-    redirect("/");
-  }
   return (
-    <main className="sm:ml-14 py-4 px-8">
+    <main className="sm:ml-14 py-4 px-8 flex flex-col min-h-screen">
       <div className="border-b border-primary pb-2 mb-2">
         <h1 className="text-xl font-bold">Task Planner</h1>
       </div>
-      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12 md:px-16"></section>
+
+      <TaskContainer />
+
     </main>
   );
 }
