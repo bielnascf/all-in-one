87370ad263c1457dc284efc2573a0a99d32ac240
Gabriel Nascimento <127701444+bielnascf@users.noreply.github.com>
2025-09-13 02:44:49 -0300

    refactor: Adicionei um spinner no loading ao clicar

--- src/app/dashboard/_components/ButtonSignOutDesktop.tsx
+++ src/app/dashboard/_components/ButtonSignOutDesktop.tsx
@@ -3,17 +3,21 @@
 import { Button } from "@/components/ui/button";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 import { authClient } from "@/lib/auth-client";
-import { LogOutIcon } from "lucide-react";
+import { Loader2Icon, LogOutIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function ButtonSignOutDesktop() {
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   const handleSignOut = async () => {
+    setLoading(true);
     await authClient.signOut({
       fetchOptions: {
         onSuccess: () => {
           router.replace("/");
+          setLoading(false);
         },
       },
     });
@@ -22,7 +26,7 @@ export default function ButtonSignOutDesktop() {
     <Tooltip>
       <TooltipTrigger asChild>
         <Button className="flex h-9 w-9 shrink-0 items-center justify-center bg-muted/40 rounded-lg transition-colors hover:bg-muted/40" onClick={handleSignOut}>
-          <LogOutIcon className="text-red-600 h-5 w-5 transition-all" />
+          {loading ? <Loader2Icon className="animate-spin w-4 h-4 text-white" /> : <LogOutIcon className="text-red-600 h-5 w-5 transition-all" />}
           <span className="sr-only">Logout</span>
         </Button>
       </TooltipTrigger>
