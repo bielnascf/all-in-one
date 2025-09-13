4d377b851d62d20da1a9f32016f661590c3d4adf
Gabriel Nascimento <127701444+bielnascf@users.noreply.github.com>
2025-09-13 01:47:30 -0300

    fix: Implementei o use client no componente

--- src/components/Gradient/index.tsx
+++ src/components/Gradient/index.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 export default function Gradient() {
  return(
   <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/4 w-[200px] h-[300px] sm:w-[600px] sm:h-[600px] bg-[#3EAC91] rounded-full blur-[200px] opacity-30 -z-10"></div>
